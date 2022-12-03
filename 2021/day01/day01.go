ebf8a71e1463f6833787d70417c5078d9ff87abf
Frankie Sadler <frankies.develop@gmail.com>
2022-12-03 13:02:33 +0000

    🚧 WIP Day 01

--- 2021/day01/day01.go
+++ 2021/day01/day01.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -24,7 +25,8 @@ func main() {
 		input = append(input, j)
 	}
 
-	fmt.Println(strconv.Itoa(SolveDay01Part1(input)))
+	fmt.Println("Part 1:", strconv.Itoa(SolveDay01Part1(input)))
+	// fmt.Println("Part 2:", strconv.Itoa(SolveDay01Part2(input)))
 }
 
 func readLines(path string) ([]string, error) {
@@ -56,4 +58,60 @@ func SolveDay01Part1(input []int) int {
 	}
 
 	return count
+}
+
+// func SolveDay01Part2(input []int) int {
+// 	count := 0
+// 	previousSum := 0
+
+// 	for i := range input {
+// 		if i >= 0 && i <= 2 {
+// 			continue
+// 		}
+
+// 		sum := input[i] + input[i-1] + input[i-2]
+
+// 		if previousSum < sum {
+// 			count++
+// 			previousSum = sum
+// 		}
+// 	}
+
+// 	return count
+// }
+
+func InputToIntSlice(input string) ([]int, error) {
+	var i []int
+	for _, line := range strings.Split(strings.TrimSuffix(input, "\n"), "\n") {
+		lineInt, err := strconv.Atoi(line)
+		if err != nil {
+			return nil, err
+		}
+		i = append(i, lineInt)
+	}
+	return i, nil
+}
+
+func SolveDay01Part2(input string) int {
+	inputInt, _ := InputToIntSlice(input)
+	avg := make([]int, len(inputInt)-2)
+	for i := range avg {
+		avg[i] += inputInt[i]
+		avg[i] += inputInt[i+1]
+		avg[i] += inputInt[i+2]
+	}
+	return calculateIncrements(avg)
+}
+
+func calculateIncrements(input []int) int {
+	var increments int
+	for i := range input {
+		if i == 0 {
+			continue
+		}
+		if input[i] > input[i-1] {
+			increments++
+		}
+	}
+	return increments
 }
--- 2021/day01/day01_test.go
+++ 2021/day01/day01_test.go
@@ -7,7 +7,7 @@ import (
 func TestSolveDay01Part1(t *testing.T) {
 	t.Run("Test SolveDay01Part1", func(t *testing.T) {
 
-		tempList := []int{199,
+		got := SolveDay01Part1([]int{199,
 			200,
 			208,
 			210,
@@ -16,11 +16,47 @@ func TestSolveDay01Part1(t *testing.T) {
 			240,
 			269,
 			260,
-			263}
-
-		got := SolveDay01Part1(tempList)
+			263})
 		expected := 7
 
+		if got != expected {
+			t.Errorf("expected '%d' but got '%d'", expected, got)
+		}
+	})
+}
+
+// func TestSolveDay01Part2(t *testing.T) {
+// 	t.Run("Test SolveDay01Part2", func(t *testing.T) {
+// 		got := SolveDay01Part2([]int{199,
+// 			200,
+// 			208,
+// 			210,
+// 			200,
+// 			207,
+// 			240,
+// 			269,
+// 			260,
+// 			263})
+// 		expected := 5
+// 		if got != expected {
+// 			t.Errorf("expected '%d' but got '%d'", expected, got)
+// 		}
+// 	})
+// }
+
+func TestSolveDay01Part2(t *testing.T) {
+	t.Run("Test SolveDay01Part2", func(t *testing.T) {
+		got := SolveDay01Part2(`199
+200
+208
+210
+200
+207
+240
+269
+260
+263`)
+		expected := 5
 		if got != expected {
 			t.Errorf("expected '%d' but got '%d'", expected, got)
 		}
